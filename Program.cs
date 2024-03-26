a5c13c2b2241215b64a6d2d75432f44f220886e9
Uliana <159609886+Programmist2003@users.noreply.github.com>
2024-03-26 13:13:15 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿// Simple Calculator
+// varribles
+int numberOne = 42;
+    int numberTwo = 119;
+    // operated add
+    int resultAdd = numberOne + numberTwo;
+   // operated subs
+    int resultSubs = numberOne - numberTwo;
+    // operated  mult
+    int resultMult = numberOne * numberTwo;
+    // operated div
+    int resultDiv = numberOne / numberTwo;
+    // operated remaind
+    int resultRemaind = numberOne % numberTwo;
+    // operated squared
+    int resultsquar = numberOne ^ numberTwo;
+    
+    // console result 
+    Console.WriteLine(resultAdd);
+    Console.ReadKey();
+    // console subs
+    Console.WriteLine(resultSubs);
+    Console.ReadKey();
+    //  console mult
+    Console.WriteLine(resultMult);
+    Console.ReadKey();
+    // console div
+    Console.WriteLine(resultDiv);
+    Console.ReadKey();
+    // console  Remaind
+    Console.WriteLine(resultRemaind);
+    Console.ReadKey();
+    // console  square
+    Console.WriteLine(resultsquar);
+    Console.ReadKey();
